081e398633b5d904e58ec97af8090b6e3856c097
pGabrielVP <paulogabrielprado@hotmail.com>
2025-08-19 21:32:09 -0300

    Improve Readability

--- src/main/java/com/mycompany/academia/services/Relatorio.java
+++ src/main/java/com/mycompany/academia/services/Relatorio.java
@@ -12,8 +12,9 @@ import java.util.Map;
 
 import com.mycompany.academia.model.dto.ExercicioWrapper;
 import com.mycompany.academia.model.dto.Sublista;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
@@ -31,51 +32,38 @@ public class Relatorio {
     private static final String REPORT_SUPERSET = "/relatorios/Listar_Superset.jasper";
     private static final String REPORT_EXERCICIOS = "/relatorios/Listar_Exercicios.jasper";
 
-    public static void imprimirRelatorio(Collection<Sublista> lista) throws JRException {
-        JasperPrint jasperPrint = get_filled_report(lista);
-        view_report(jasperPrint);
+    public static void imprimirRelatorio(Collection<Sublista> lista) {
+        try {
+            JRBeanCollectionDataSource datasource = new JRBeanCollectionDataSource(lista);
+            JasperReport relatorioPrincipal = loadReport(REPORT_PRINCIPAL);
+
+            HashMap<String, Object> parametros = new HashMap();
+            JasperReport relatorioSuperSet = loadReport(REPORT_SUPERSET);
+            JasperReport relatorioExercicios = loadReport(REPORT_EXERCICIOS);
+            parametros.put("subreportSuperset", relatorioSuperSet);
+            parametros.put("subreportExercicios", relatorioExercicios);
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(relatorioPrincipal, parametros, datasource);
+            JasperViewer jasperViewer = new JasperViewer(jasperPrint, false);
+            jasperViewer.setTitle("Rotina");
+            jasperViewer.setVisible(true);
+        } catch (JRException ex) {
+            Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    public static void imprimirRelatorio(ArrayList<String> titulos, ArrayList<ArrayList<ExercicioWrapper>> exercicios, ArrayList<HashMap<ExercicioWrapper, ExercicioWrapper>> superset) throws JRException {
-        Collection<Sublista> lista = get_dataset(titulos, exercicios, superset);
-        JasperPrint jasperPrint = get_filled_report(lista);
-        view_report(jasperPrint);
+    public static void imprimirRelatorio(ArrayList<String> titulos, ArrayList<ArrayList<ExercicioWrapper>> exercicios, ArrayList<HashMap<ExercicioWrapper, ExercicioWrapper>> superset) {
+        Collection<Sublista> lista = asCollection(titulos, exercicios, superset);
+        imprimirRelatorio(lista);
     }
 
-    private static JasperPrint get_filled_report(Collection<Sublista> lista) throws JRException {
-        JRBeanCollectionDataSource datasource = new JRBeanCollectionDataSource(lista);
-        JasperReport principal_jasper = get_report_object(REPORT_PRINCIPAL);
-        HashMap<String, Object> parametros = set_parametros();
-        JasperPrint jasperPrint = JasperFillManager.fillReport(principal_jasper, parametros, datasource);
-        return jasperPrint;
+    private static JasperReport loadReport(String report_location) throws JRException {
+        InputStream inputStream = Relatorio.class.getResourceAsStream(report_location);
+        JasperReport jasperReport = (JasperReport) JRLoader.loadObject(inputStream);
+        return jasperReport;
     }
 
-    private static HashMap<String, Object> set_parametros() throws JRException {
-        HashMap<String, Object> parametros = new HashMap();
-        JasperReport superset_jasper = get_report_object(REPORT_SUPERSET);
-        JasperReport exercicios_jasper = get_report_object(REPORT_EXERCICIOS);
-        parametros.put("subreportSuperset", superset_jasper);
-        parametros.put("subreportExercicios", exercicios_jasper);
-        return parametros;
-    }
-
-    private static JasperReport get_report_object(String report_location) throws JRException {
-        InputStream report_input_stream = Relatorio.class.getResourceAsStream(report_location);
-        JasperReport report_object = (JasperReport) JRLoader.loadObject(report_input_stream);
-        return report_object;
-    }
-
-    private static void view_report(JasperPrint jasperPrint) {
-        JasperViewer jasperViewer = new JasperViewer(jasperPrint, false);
-        jasperViewer.setTitle("ROTINA");
-        jasperViewer.setVisible(true);
-    }
-
-    private static void export_to_pdf(JasperPrint jasperPrint) throws JRException {
-        JasperExportManager.exportReportToPdf(jasperPrint);
-    }
-
-    private static Collection<Sublista> get_dataset(ArrayList<String> titulos, ArrayList<ArrayList<ExercicioWrapper>> exercicios, ArrayList<HashMap<ExercicioWrapper, ExercicioWrapper>> superset) {
+    private static Collection<Sublista> asCollection(ArrayList<String> titulos, ArrayList<ArrayList<ExercicioWrapper>> exercicios, ArrayList<HashMap<ExercicioWrapper, ExercicioWrapper>> superset) {
         Collection<Sublista> lista = new ArrayList<>();
 
         for (int i = 0; i < exercicios.size(); i++) {
